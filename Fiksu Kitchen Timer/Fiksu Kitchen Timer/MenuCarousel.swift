51b67e8fe129ac73947dad95a17b932ea79a9e36
Dr. Alf Bae <alfpooh@gmail.com>
2016-08-14 23:57:20 +0300

    F! button titles are now parsed from JSON!

--- Fiksu Kitchen Timer/Fiksu Kitchen Timer/MenuCarousel.swift
+++ Fiksu Kitchen Timer/Fiksu Kitchen Timer/MenuCarousel.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import SwiftyJSON
 
 @IBDesignable
 
@@ -27,7 +28,7 @@ class MenuViewController: UIViewController, ZCarouselDelegate, ZCarouselTitleDel
     let noodleRecipe: [String] = ["Egg noodle","Instant ramen","Somen"]
     let pastaRecipe: [String] = [ "Farfalle","Fusili","Macaroni","Conchiglie","Spagehtti","Tagliatelle"]
     let roastRecipe: [String] = ["Beef","Chicken","Salmon"]
-    let MenuArray: [String] = ["PASTA","ROAST","ALL","BOILING","NOODLE"]
+    var MenuArray: [String] = ["PASTA","ROAST","ALL","BOILING","NOODLE"]
     let TestArray: [String] = ["1", "2", "3", "4","5"]
     let recipeGroup: [String] = ["boilingRecipe","noodleRecipe","pastaRecipe","roastRecipe"]
     let ReplacingArray: [String]! =  nil
@@ -35,6 +36,31 @@ class MenuViewController: UIViewController, ZCarouselDelegate, ZCarouselTitleDel
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //json
+        if let path = NSBundle.mainBundle().pathForResource("FiksuTimerRecipes", ofType: "json") {
+            do {
+                let data = try NSData(contentsOfURL: NSURL(fileURLWithPath: path), options: NSDataReadingOptions.DataReadingMappedIfSafe)
+                let jsonObj = JSON(data: data)
+                if jsonObj != JSON.null {
+                    
+                    //print("jsonData:\(jsonObj)")
+                    // playing with json
+              print("test: \(jsonObj["cooktype"])")
+                    MenuArray.removeAll()
+                    let count = jsonObj["cooktype"].count - 1
+                    for i in 0...count {
+                    MenuArray.append(jsonObj["cooktype"][i].string!)
+                    }
+                } else {
+                    print("could not get json from file, make sure that file contains valid json.")
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        } else {
+            print("Invalid filename/path.")
+        }
 
         //creating menu buttons
         menu = ZCarouselTitle(frame: CGRect( x: (self.view.frame.size.width/5),
