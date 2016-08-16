0479c22135f917fdf19287949ed72d918da0f1be
Dr. Alf Bae <alfpooh@gmail.com>
2016-08-16 13:18:10 +0300

    targetTime is now connected with JSON

--- Fiksu Kitchen Timer/Fiksu Kitchen Timer/ViewController.swift
+++ Fiksu Kitchen Timer/Fiksu Kitchen Timer/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import SwiftyJSON
 
 
 class ViewController: UIViewController {
@@ -20,7 +21,7 @@ class ViewController: UIViewController {
     var timerRewindplayer: AVAudioPlayer!
     var sayCount = AVSpeechUtterance(string: "")
     //
-    let targetTime = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    var targetTimes: Array = [0]
     var isPaused = false
     var isTimerOn = false
     var pausedTime: NSTimeInterval!
@@ -72,15 +73,46 @@ class ViewController: UIViewController {
         selectedrecipeindex = parseTransferedindex(transferText)
         let timerindex = selectedrecipeindex
         
+        //Get targetTIme from Json
+        getTargetTime()
+        
         // set target.
-        currentTargetmin = Double(targetTime[timerindex])
-        if targetTime[timerindex] < 10 {
-            targetTimeLabel.text = "0\(targetTime[timerindex]):00"
+        currentTargetmin = Double(targetTimes[timerindex])
+        if targetTimes[timerindex] < 10 {
+            targetTimeLabel.text = "0\(targetTimes[timerindex]):00"
         } else {
-            targetTimeLabel.text = "\(targetTime[timerindex]):00"}
+            targetTimeLabel.text = "\(targetTimes[timerindex]):00"}
         
     }
     
+    func getTargetTime() {
+        //json
+        if let path = NSBundle.mainBundle().pathForResource("FiksuTimerRecipes", ofType: "json") {
+            do {
+                let data = try NSData(contentsOfURL: NSURL(fileURLWithPath: path), options: NSDataReadingOptions.DataReadingMappedIfSafe)
+                let jsonObj = JSON(data: data)
+                if jsonObj != JSON.null {
+                    
+                    
+                    //Cooktype from Jason!
+                    targetTimes.removeAll()
+                    let count = jsonObj["Recipes"].count - 1
+                    for i in 0...count {
+                        targetTimes.append(Int(jsonObj["Recipes"][i]["cooktime"].string!)!)
+                    }
+                    print ("\(targetTimes)")
+                }
+                else {
+                    print("could not get json from file, make sure that file contains valid json.")
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        } else {
+            print("Invalid filename/path.")
+        }
+        
+    }
     func parseTransferedindex (transfered: String) -> Int{
         //need to analyze transfered here
         let strSplit = transfered.characters.split(":")
--- FiksuCalculator/FiksuCalculator/ViewController.swift
+++ FiksuCalculator/FiksuCalculator/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import iAd
 import AVFoundation
 
 extension Double {
